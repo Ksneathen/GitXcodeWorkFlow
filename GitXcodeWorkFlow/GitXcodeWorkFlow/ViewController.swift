ce6bb3cebada46e74361d7e7dc45951d7fc6bc48
Kaylyn Sneathen <kss9tf@mail.missouri.edu>
2017-02-08 12:41:38 -0600

    Created a Xcode Project

---
+++ GitXcodeWorkFlow/GitXcodeWorkFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcodeWorkFlow
+//
+//  Created by Kaylyn Sneathen on 2/8/17.
+//  Copyright © 2017 Kaylyn Sneathen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
